cef055d3bce3b580e230f731ce999269e004d949
Salvatore <sformica505@gmail.com>
2026-08-26 18:37:42 +0200

    feat: add database infrastructure

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     spotify_client_id: str
     spotify_client_secret: str
     spotify_redirect_uri: str
+    database_url: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
